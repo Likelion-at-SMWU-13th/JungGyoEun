f1f1a152f19a9ec9086dc3241adf9056b426ad8a
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-05-20 20:42:00 +0900

    컴포넌트 실습 완료

--- 프론트/FEweek04/week04/src/App.jsx
+++ 프론트/FEweek04/week04/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import LionCard1 from "./components/LionCard1.jsx";
+import LionCard2 from "./components/LionCard2.jsx";
+import LionCard3 from "./components/LionCard3.jsx";
 
 function App() {
-  return <></>;
+  const liar = "가짜 아기사자";
+  return (
+    <>
+      <h1>{liar}를 찾아라!</h1>
+      <LionCard1 />
+      <LionCard2 />
+      <LionCard3 />
+    </>
+  );
 }
 
 export default App;
