6f42cb6f8f9e12991eca94bfc1771f3c59f70987
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-06-24 15:53:24 +0900

    [Feat] useState를 통한 상태 관리 구현

--- 프론트/FEweek05/week05/src/App.jsx
+++ 프론트/FEweek05/week05/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Hello from "./components/Hello";
+import Info from "./components/info";
 
 function App() {
   return (
     <>
-      <Hello name={"숙멋사"} />
+      <Info />
     </>
   );
 }
