5615d5f3c407b4817e9ffe5b5cc50c9f253d6128
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-06-24 16:57:33 +0900

    [Feat] 영화 목록 구

--- 프론트/FEweek05/week05/src/App.jsx
+++ 프론트/FEweek05/week05/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import Hello from "./components/Hello";
 import Info from "./components/info";
+import Movie from "./components/Movie";
 
 function App() {
   return (
     <>
-      <Info />
+      <Movie />
     </>
   );
 }
