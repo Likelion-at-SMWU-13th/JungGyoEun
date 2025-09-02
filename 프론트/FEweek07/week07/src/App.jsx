10bfa41b06139abbf493a819d167e30fb10b7920
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-09-02 20:02:33 +0900

    [Setting] 리액트 앱 초기 세팅

--- 프론트/FEweek07/week07/src/App.jsx
+++ 프론트/FEweek07/week07/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <Wrapper>
-      <Title onClick={() => navigate("/")}>숙멋 프론트의 TMI 게시판 🦁</Title>
+      <Title onClick={() => navigate("/")}>교은 사자의 TMI 게시판 🦁</Title>
       <Outlet />
     </Wrapper>
   );
