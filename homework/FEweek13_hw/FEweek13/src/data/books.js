c3fe8cf34db767260dc5ff0c36d0cd13a65bfa5b
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-09-15 05:36:32 +0900

    [Feat] 더미데이터 books.js 추가

--- homework/FEweek13_hw/FEweek13/src/data/books.js
+++ homework/FEweek13_hw/FEweek13/src/data/books.js
@@ -1,70 +1,100 @@
 export const BOOKS = [
   {
+    id: 1,
     title: "가공범",
     author: "히가시노 게이고",
+    publisher: "북다",
+    date: "2025.07.21",
     price: 19800,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791170612759.jpg",
   },
   {
+    id: 2,
     title: "혼모노",
     author: "성해나",
+    publisher: "창비",
+    date: "2025.03.28",
     price: 18000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9788936439743.jpg",
   },
   {
+    id: 3,
     title: "다크 심리학",
     author: "다크 사이드 프로젝트",
+    publisher: "어센딩",
+    date: "2025.07.31",
     price: 21900,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791198754080.jpg",
   },
   {
+    id: 4,
     title: "자몽살구클럽",
     author: "한로로",
+    publisher: "어센틱",
+    date: "2025.07.25",
     price: 12000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791199305304.jpg",
   },
   {
+    id: 5,
     title: "모순",
     author: "양귀자",
+    publisher: "쓰다",
+    date: "2013.04.01",
     price: 13000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9788998441012.jpg",
   },
   {
+    id: 6,
     title: "편안함의 습격",
     author: "마이클 이스터",
+    publisher: "수오서재",
+    date: "2025.06.24",
     price: 22000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791193238691.jpg",
   },
   {
+    id: 7,
     title: "박곰희 연금 부자 수업",
     author: "박곰희",
+    publisher: "인플루엔셜",
+    date: "2025.06.25",
     price: 189000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791168342941.jpg",
   },
   {
+    id: 8,
     title: "호의에 대하여",
     author: "문형배",
+    publisher: "김영사",
+    date: "2025.08.28",
     price: 18800,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791173323027.jpg",
   },
   {
+    id: 9,
     title: "류수영의 평생 레시피",
     author: "류수영",
+    publisher: "세미콜론",
+    date: "2025.06.25",
     price: 25000,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791194087700.jpg",
   },
   {
+    id: 10,
     title: "안녕이라 그랬어",
     author: "김애란",
+    publisher: "문학동네",
+    date: "2025.06.20",
     price: 16800,
     imageUrl:
       "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791141602376.jpg",
