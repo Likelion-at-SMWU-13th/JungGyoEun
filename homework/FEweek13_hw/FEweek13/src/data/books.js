be700051deef32229bf7518e37fc569b799068a4
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-09-15 02:27:04 +0900

    [Feat] 더미데이터 생성

---
+++ homework/FEweek13_hw/FEweek13/src/data/books.js
@@ -0,0 +1,72 @@
+export const BOOKS = [
+  {
+    title: "가공범",
+    author: "히가시노 게이고",
+    price: 19800,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791170612759.jpg",
+  },
+  {
+    title: "혼모노",
+    author: "성해나",
+    price: 18000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9788936439743.jpg",
+  },
+  {
+    title: "다크 심리학",
+    author: "다크 사이드 프로젝트",
+    price: 21900,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791198754080.jpg",
+  },
+  {
+    title: "자몽살구클럽",
+    author: "한로로",
+    price: 12000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791199305304.jpg",
+  },
+  {
+    title: "모순",
+    author: "양귀자",
+    price: 13000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9788998441012.jpg",
+  },
+  {
+    title: "편안함의 습격",
+    author: "마이클 이스터",
+    price: 22000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791193238691.jpg",
+  },
+  {
+    title: "박곰희 연금 부자 수업",
+    author: "박곰희",
+    price: 189000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791168342941.jpg",
+  },
+  {
+    title: "호의에 대하여",
+    author: "문형배",
+    price: 18800,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791173323027.jpg",
+  },
+  {
+    title: "류수영의 평생 레시피",
+    author: "류수영",
+    price: 25000,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791194087700.jpg",
+  },
+  {
+    title: "안녕이라 그랬어",
+    author: "김애란",
+    price: 16800,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9791141602376.jpg",
+  },
+];
